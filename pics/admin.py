486bb39059bbc24a2dd367e07fd94573624c58a2
edumorris <eduuwachira@gmail.com>
2020-08-02 11:32:55 +0300

    Test images upload

--- pics/admin.py
+++ pics/admin.py
@@ -3,10 +3,10 @@ from django.contrib import admin
 from .models import Images, Category, Location, Subs
 
 # Register your models here.
-class PictorialAdmin(admin.ModelAdmin):
-    filter_horizontal =('Category, Location,')
+class ImagesAdmin(admin.ModelAdmin):
+    filter_horizontal =('category',)
     
 admin.site.register(Location)
 admin.site.register(Category)
-admin.site.register(Images)
+admin.site.register(Images, ImagesAdmin)
 admin.site.register(Subs)
