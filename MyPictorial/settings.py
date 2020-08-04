3b91e9e9f2d493fdd61ac53c000b076124801752
edumorris <eduuwachira@gmail.com>
2020-08-04 10:18:26 +0300

    Django heroku & python-decouple install

--- MyPictorial/settings.py
+++ MyPictorial/settings.py
@@ -52,6 +52,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+MIDDLEWARE_CLASSES = (
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
 ROOT_URLCONF = 'MyPictorial.urls'
 
 TEMPLATES = [
@@ -128,6 +132,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media' )
